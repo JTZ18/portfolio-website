1692a2d1d78e53170651f14e241c91c0fd5e0ad9
JTZ18 <jon.taylorlim99@gmail.com>
2022-02-08 18:59:50 +0800

    remove urn, room2, room, buildingobj, buildinggltf, building fbx and custom shader material and wave fucntion from newapp

--- src/NewApp.js
+++ src/NewApp.js
@@ -104,18 +104,6 @@ function BackdropWithShader() {
 
 }
 
-function Urns({...props }) {
-  const { scene, nodes, animations } = useLoader(GLTFLoader,'/gen_urn_lores_mod.glb')
-  const { actions } = useAnimations(animations, scene)
-  console.log(scene)
-  console.log(nodes)
-  console.log(actions)
-  // useEffect(() => void (actions['CameraAction'].play().paused = true), [actions])
-  // useFrame((state) => {
-  //   actions["CameraAction"].time = THREE.MathUtils.lerp(actions["CameraAction"].time, actions["CameraAction"].getClip().duration * scroll.current, 0.05)
-  // })
-  return <primitive object={scene} {...props} />
-}
 
   function Smoke({IconPosition, IconSize, ...props }) {
 
@@ -131,48 +119,6 @@ function Urns({...props }) {
     )
 }
 
-function Scene({ ...props }) {
-  // This hook gives you offets, ranges and other useful things
-  const { scene, nodes, animations } = useLoader(GLTFLoader,'/town.glb')
-  console.log(animations)
-  console.log(scene)
-  //for shadows
-  return <primitive object={scene} {...props} />
-}
-
-function Room({ ...props }) {
-    // This hook gives you offets, ranges and other useful things
-    const fbx = useFBX('/Room.fbx')
-    console.log(fbx)
-    //for shadows
-    return <primitive object={fbx} {...props} />
-  }
-
-function Room2({ ...props }) {
-// This hook gives you offets, ranges and other useful things
-    const { scene, nodes, animations } = useLoader(GLTFLoader,'/isometric-room.glb')
-    console.log(animations)
-    console.log(scene)
-    //for shadows
-    return <primitive object={scene} {...props} />
-}
-
-function Buildingobj({ ...props }) {
-  // This hook gives you offets, ranges and other useful things
-      const obj = useLoader(OBJLoader,'/building.obj')
-      console.log(obj)
-      //for shadows
-      return <primitive object={obj} {...props} />
-  }
-
-function BuildingGLTF({ ...props }) {
-  // This hook gives you offets, ranges and other useful things
-  const { scene, nodes, animations } = useLoader(GLTFLoader,'/building.gltf')
-  console.log(animations)
-  console.log(scene)
-  //for shadows
-  return <primitive object={scene} {...props} />
-}
 
 function BuildingGLB({ ...props }) {
   // This hook gives you offets, ranges and other useful things
@@ -183,141 +129,8 @@ function BuildingGLB({ ...props }) {
   return <primitive object={scene} {...props} />
 }
 
-function BuildingFBX({ ...props }) {
-  // This hook gives you offets, ranges and other useful things
-  const fbx = useFBX('/building.fbx')
-  console.log(fbx)
-  //for shadows
-  return <primitive object={fbx} {...props} />
-}
 
-const customShaderMaterial = shaderMaterial(
-  // Uniform
-  {
-    time: 0
-  },
-  //vertex shaderMateria
-  glsl`
-  varying vec2 vUv;
-  void main() {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectionPosition = projectionMatrix * viewPosition;
-    gl_Position = projectionPosition;
-    vUv = uv;
-  }
-  `,
-  //fragmen shaderMaterial
-  glsl`
-  #ifdef GL_ES
-  precision mediump float;
-  #endif
-  
-  varying vec2 vUv;
-  varying float vUTime;
-  
-  const mat2 m = mat2( 0.80,  0.60, -0.60,  0.80 );
-  
-  float noise( vec2 p )
-  {
-    return sin(p.x)*sin(p.y);
-  }
-  
-  float fbm4( vec2 p )
-  {
-      float f = 0.0;
-      f += 0.5000*noise( p ); p = m*p*2.02;
-      f += 0.2500*noise( p ); p = m*p*2.03;
-      f += 0.1250*noise( p ); p = m*p*2.01;
-      f += 0.0625*noise( p );
-      return f/0.9375;
-  }
-  
-  float fbm6( vec2 p )
-  {
-      float f = 0.0;
-      f += 0.500000*(0.5+0.5*noise( p )); p = m*p*2.02;
-      f += 0.250000*(0.5+0.5*noise( p )); p = m*p*2.03;
-      f += 0.125000*(0.5+0.5*noise( p )); p = m*p*2.01;
-      f += 0.062500*(0.5+0.5*noise( p )); p = m*p*2.04;
-      f += 0.031250*(0.5+0.5*noise( p )); p = m*p*2.01;
-      f += 0.015625*(0.5+0.5*noise( p ));
-      return f/0.96875;
-  }
-  
-  vec2 fbm4_2( vec2 p )
-  {
-      return vec2(fbm4(p), fbm4(p+vec2(7.8)));
-  }
-  
-  vec2 fbm6_2( vec2 p )
-  {
-      return vec2(fbm6(p+vec2(16.8)), fbm6(p+vec2(11.5)));
-  }
-  
-  
-  float func( vec2 q, out vec4 ron )
-  {
-      q += 0.03*sin( vec2(0.27,0.23)*vUTime + length(q)*vec2(4.1,4.3));
-  
-    vec2 o = fbm4_2( 0.9*q );
-  
-      o += 0.04*sin( vec2(0.12,0.14)*vUTime + length(o));
-  
-      vec2 n = fbm6_2( 3.0*o );
-  
-    ron = vec4( o, n );
-  
-      float f = 0.5 + 0.5*fbm4( 1.8*q + 6.0*n );
-  
-      return mix( f, f*f*f*3.5, f*abs(n.x) );
-  }
-  
-  void main() 
-  {
-      // vec2 p = (2.0*vUv-iResolution.xy)/iResolution.y;
-      vec2 p = -1.0 + 3.0 *vUv;
-      float e = 2.0;
-  
-      vec4 on = vec4(0.0);
-      float f = func(p, on);
-  
-      vec3 col = vec3(0.0);
-      col = mix( vec3(0.2,0.1,0.4), vec3(0.3,0.05,0.05), f );
-      col = mix( col, vec3(1,0.98,0.98), dot(on.zw,on.zw) );
-      col = mix( col, vec3(0.73,0.1,0.32), 0.2 + 0.5*on.y*on.y );
-      col = mix( col, vec3(0.09,0.26,0.1), 0.5*smoothstep(1.2,1.3,abs(on.z)+abs(on.w)) );
-      col = clamp( col*f*2.0, 0.0, 1.0 );
-      
-  
-      // manual derivatives - better quality, but slower
-      vec4 kk;
-     vec3 nor = normalize( vec3( func(p+vec2(e,0.0),kk)-f, 
-                                  2.0*e,
-                                  func(p+vec2(0.0,e),kk)-f ) );
-      
-      gl_FragColor = vec4( col, 1.0 );
-  }
-  `
 
-);
-extend({customShaderMaterial})
-
-const Wave = () => {
-  const ref = useRef();
-  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
-
-  const [image] = useLoader(THREE.TextureLoader, [
-    "https://images.unsplash.com/photo-1604011092346-0b4346ed714e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
-  ]);
-
-  return (
-    <mesh>
-      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
-      <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} />
-    </mesh>
-  );
-};
 
 
 
